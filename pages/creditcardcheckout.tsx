5dff6ca5773314a63a7b3658e8f375323b46cd78
Anne Shackelford <49678301+anneshackelford@users.noreply.github.com>
2022-02-27 17:23:23 +0100

    Fix lost css

--- pages/creditcardcheckout.tsx
+++ pages/creditcardcheckout.tsx
@@ -1,16 +1,19 @@
 import styled from 'styled-components';
 
 const Layout = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
+  /* display: flex; */
+  /* flex-direction: column; */
+  /* justify-content: center; */
+  /* align-items: center; */
+  /* height: 100vh; */
+  height: 600px;
   font-size: 48px;
+  margin: 200px auto;
+  text-align: center;
 `;
 
-function CreditCardCheckout() {
-  return (<div>Work in progress for Credit Card Checkout!</div>);
-}
+const CreditCardCheckout = () => {
+  return <Layout>Work in progress for Credit Card Checkout!</Layout>;
+};
 
 export default CreditCardCheckout;
