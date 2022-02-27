4e212c5017aaf46d3bb8bdf8476c56f39c20bf96
Anne Shackelford <49678301+anneshackelford@users.noreply.github.com>
2022-02-27 17:11:40 +0100

    Fix lost  CSS

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,16 +1,16 @@
 import styled from 'styled-components';
 
-// const Layout = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: center;
-//   align-items: center;
-//   height: 100vh;
-//   font-size: 48px;
-// `;
+const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  font-size: 48px;
+`;
 
 function SignUp() {
-  return <>Work in progress for Sign Up!</>;
+  return (<Layout>Work in progress for Sign Up!</Layout>);
 }
 
 export default SignUp;
