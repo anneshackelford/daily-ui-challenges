b396ace622fb1bef2ddf43fc60e76876c5cb4f1f
Anne Shackelford <49678301+anneshackelford@users.noreply.github.com>
2022-02-27 17:04:22 +0100

    Fix disappearing  css

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,16 +1,16 @@
 import styled from 'styled-components';
 
-const Layout = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  font-size: 48px;
-`;
+// const Layout = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   align-items: center;
+//   height: 100vh;
+//   font-size: 48px;
+// `;
 
 function SignUp() {
-  return <Layout>Work in progress for Sign Up!</Layout>;
+  return <>Work in progress for Sign Up!</>;
 }
 
 export default SignUp;
