715cdc14138f4bd54219a35f633e19f6230dec73
Anne Shackelford <49678301+anneshackelford@users.noreply.github.com>
2022-02-27 10:57:08 +0100

    Setup landing page

---
+++ components/DailyUI.tsx
@@ -0,0 +1,65 @@
+import styled, { css } from 'styled-components';
+
+interface DailyProps {
+    backgroundColor: string;
+}
+
+const Daily = styled.div<DailyProps>`
+  height: 480px;
+  width: 640px;
+  background-color: ${(props) => props.backgroundColor || 'none'};
+  border: 1px solid #ffffff;
+  > img {
+    margin: 73px 62px auto;
+    width: 515px;
+  }
+`;
+
+const DailyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  margin-top: 73px;
+  width: 515px;
+`;
+
+const DailyBottomRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 0 8px;
+`;
+
+const DailyLabel = styled.label`
+  font-family: Clash Display;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 30px;
+  text-transform: uppercase;
+  color: ${(props) => props.color || '#ffffff'};
+`;
+
+interface DailyUIProps {
+  backgroundColor: string;
+  number: string;
+  name: string;
+  color: string;
+}
+
+export const DailyUI = ({ backgroundColor, number, name, color }: DailyUIProps) => {
+  return (
+    <Daily backgroundColor={backgroundColor}>
+      <DailyContainer>
+        <img
+          src={`images/${number}.png`}
+          alt={`Daily UI Challenge ${number} ${name}`}
+        />
+        <DailyBottomRow>
+          <DailyLabel color={color}>{name}</DailyLabel>
+          <DailyLabel color={color}>{number}</DailyLabel>
+        </DailyBottomRow>
+      </DailyContainer>
+    </Daily>
+  );
+};
---
+++ pages/creditcard.tsx
@@ -0,0 +1,5 @@
+function CreditCard() {
+    return <div>Credit Card</div>
+  }
+  
+  export default CreditCard;
