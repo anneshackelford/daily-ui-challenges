83bf9bdaa222d1594a4f86baaa9fc238b0f7ccf9
Anne Shackelford <49678301+anneshackelford@users.noreply.github.com>
2022-02-27 17:32:36 +0100

    Fix lost css

--- pages/creditcardcheckout.tsx
+++ pages/creditcardcheckout.tsx
@@ -1,19 +1,20 @@
 import styled from 'styled-components';
 
 const Layout = styled.div`
-  /* display: flex; */
-  /* flex-direction: column; */
-  /* justify-content: center; */
-  /* align-items: center; */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   /* height: 100vh; */
-  height: 600px;
   font-size: 48px;
-  margin: 200px auto;
-  text-align: center;
 `;
 
 const CreditCardCheckout = () => {
-  return <Layout>Work in progress for Credit Card Checkout!</Layout>;
+  return (
+    <Layout>
+      <Layout>Work in progress for Credit Card Checkout!</Layout>{' '}
+    </Layout>
+  );
 };
 
 export default CreditCardCheckout;
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,19 +1,20 @@
 import styled from 'styled-components';
 
 const Layout = styled.div`
-  /* display: flex; */
-  /* flex-direction: column; */
-  /* justify-content: center; */
-  /* align-items: center; */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   /* height: 100vh; */
-  height: 600px;
   font-size: 48px;
-  margin: 200px auto;
-  text-align: center;
 `;
 
 const SignUp = () => {
-  return <Layout>Work in progress for Sign Up!</Layout>;
+  return (
+    <Layout>
+      <Layout>Work in progress for Sign Up!</Layout>
+    </Layout>
+  );
 };
 
 export default SignUp;
