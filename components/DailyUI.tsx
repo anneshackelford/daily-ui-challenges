11f517551d7d1bd326db81cf119adce1af55175e
Anne Shackelford <49678301+anneshackelford@users.noreply.github.com>
2022-02-27 12:34:13 +0100

    make landing page adjustments

--- components/DailyUI.tsx
+++ components/DailyUI.tsx
@@ -5,20 +5,20 @@ interface DailyProps {
 }
 
 const Daily = styled.div<DailyProps>`
-  height: 480px;
-  width: 640px;
+  /* height: 480px;
+  width: 640px; */
   background-color: ${(props) => props.backgroundColor || 'none'};
   border: 1px solid #ffffff;
   > img {
-    margin: 73px 62px auto;
-    width: 515px;
+    /* margin: 73px 62px; */
+    /* width: 515px; */
   }
   @media screen and (max-width: 600px) {
-    width: 360px;
-    height: 270px;
+    /* width: 360px; */
+    /* height: 270px; */
     > img {
-      margin: 0 auto;
-      width: 300px;
+      /* margin: 0 auto; */
+      /* width: 300px; */
     }
   }
 `;
@@ -27,11 +27,11 @@ const DailyContainer = styled.div`
   display: flex;
   flex-direction: column;
   margin: 0 auto;
-  margin-top: 73px;
-  width: 515px;
+  margin: 73px 62px;
+  /* width: 515px; */
   @media screen and (max-width: 600px) {
     margin-top: 36px;;
-    width: 300px;
+    /* width: 300px; */
   }
 `;
 
